bc96ce9b934ebaabb39ce89be31bf64eaaa92346
Samuel Josefano Kaloh <kalohsamuel@gmail.com>
2024-10-16 12:31:48 +0700

    update: header

--- resources/views/hotel-page.blade.php
+++ resources/views/hotel-page.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel Reviews | ETS Web Programming</title>
+    <title>Hotel Reviews | Hotel Reviews</title>
 </head>
 <body>
     <article>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/Register.css">
-    <title>Register | ETS Web Programming</title>
+    <title>Register | Hotel Reviews</title>
 </head>
 <body>
     <div class="container-1">
